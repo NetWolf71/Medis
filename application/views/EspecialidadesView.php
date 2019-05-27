06f2a93e92f12a9ae6416bb7f89f998d5d3e9bdb
Luis <luis.garcia@umce.cl>
2019-05-27 11:06:07 -0400

    Aplico estilos de los demás mantenedores que desarrolló Rony

--- application/views/EspecialidadesView.php
+++ application/views/EspecialidadesView.php
@@ -2,6 +2,15 @@
 	// Inicio HTML
 	$this->load->view('sis_header');
 ?>
+<style type="text/css">
+	th{
+		text-align: center;
+		white-space:nowrap;
+	}
+</style>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+
 <div class="container">
 	<h4>MANTENEDOR DE ESPECIALIDADES</br></h4>
 </div>
@@ -26,26 +35,29 @@
 	<?= form_close() ?>
 	</br></br></br></br>
 	<table class="table table-hover table-bordered" id="tbl_datos">
-		<tr>
-			<th>Código de Especialidad</th>
-			<th>Nombre de Especialidad</th>
-			<th colspan="2">Acciones</th>
-		</tr>
-		<tr>
-		<?php
-			if ($consulta) {
-			foreach ($consulta->result() as $esp) { ?>
-				<tr>
-					<td><?= $esp->id_especialidad; ?></td>
-					<td><?= $esp->nombre ?></td>
-					<td style="white-space: nowrap;" >
+		<thead>	
+			<tr>
+				<th>Código de Especialidad</th>
+				<th>Nombre de Especialidad</th>
+				<th>Acciones</th>
+			</tr>
+		</thead>
+		<tbody>
+				<?php
+					if ($consulta) {
+						foreach ($consulta->result() as $esp) {
+				?>
+			<tr>
+				<td><?= $esp->id_especialidad; ?></td>
+				<td><?= $esp->nombre ?></td>
+				<td align="center" style="white-space:nowrap;">
 					<button class="btn btn-sm btn-warning" type="button" onclick='window.location.href="<?=base_url('index.php/EspecialidadesController/editar/'.$esp->id_especialidad);?>";'  >
-														<em class="glyphicon glyphicon-remove"></em> Modificar
-												</button>
-												<button class="btn btn-sm btn-danger" type="button" onclick='window.location.href="<?=base_url('index.php/EspecialidadesController/eliminar/'.$esp->id_especialidad);?>";'>
-													<em class="glyphicon glyphicon-plus"></em> Eliminar
-												</button>
-				</tr>
+						<em class="glyphicon glyphicon-remove"></em> Modificar
+					</button>
+					<button class="btn btn-sm btn-danger" type="button" onclick='window.location.href="<?=base_url('index.php/EspecialidadesController/eliminar/'.$esp->id_especialidad);?>";'>
+						<em class="glyphicon glyphicon-plus"></em> Eliminar
+					</button>
+			</tr>
 			<?php }
 		}else{
 			echo "<tr>
@@ -53,9 +65,30 @@
 				</tr>";
 		}
 		?>
+		</tbody>
 	</table>
 </div>
 </br></br></br>
+<script type="text/javascript">
+		$(document).ready(function() {
+			$('#tbl_datos').DataTable( {
+				"language": {
+					"lengthMenu": "Mostrar _MENU_ &nbsp;registros",
+					"zeroRecords": "Nothing found - sorry",
+					"info": "Página _PAGE_ de _PAGES_",
+					"infoEmpty": "No records available",
+					"infoFiltered": "(filtered from _MAX_ total records)",
+					"paginate": {
+						"first":      "Primero",
+						"last":       "Último",
+						"next":       "Siguiente",
+						"previous":   "Anterior"
+					},
+					"search":         "Búsqueda Rápida:",
+				}
+			} );
+		} );		
+	</script>
 <?php
 	// Fin HTML
 	$this->load->view('sis_footer');
--- application/views/sis_footer.php
+++ application/views/sis_footer.php
@@ -5,15 +5,15 @@
         <div class="row">
         	<div class="col-md-3 footer-brand animated fadeInLeft">
             	<h2>MEDIS™</h2>
-                <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
-                <p>© 2014 BS3 UI Kit, All rights reserved</p>
+                <p>Medis es una empresa chilena al servicio de las necesidades de las personas y cuyo fin es centralizar las búsquedas de horas médicas</p>
+                <p>© 2019 Mulholland Drive 66, Todos los derechos reservados</p>
             </div>
         	<div class="col-md-3 footer-nav animated fadeInUp">
                     <ul class="list">
                         <li><a href="#">Sobre MEDIS™</a></li>
                         <li><a href="#">Contáctenos</a></li>
                         <li><a href="#">Términos & Condiciones</a></li>
-                        <li><a class="nav-link" href="<?=base_url('index.php/UsuariosController')?>">Gestión Usuarios</a></li>
+                        <!-- <li><a class="nav-link" href="<?=base_url('index.php/UsuariosController')?>">Gestión Usuarios</a></li> -->
                     </ul>
             </div>
         	<div class="col-md-3 footer-social animated fadeInDown">
@@ -26,13 +26,13 @@
             </div>
         	<div class="col-md-3 footer-ns animated fadeInRight">
             	<h4>Newsletter</h4>
-                <p>Para suscribirse a nuestro Newsletter MEDIS™ envíenos un mail a <strong><a href="mailto:newsletter@medis">newsletter@medis.cl</a></strong></p>
+                <p>Para suscribirse a nuestro Newsletter MEDIS™ envíenos un mail a <strong><a href="mailto:newsletter@medis.cl">newsletter@medis.cl</a></strong></p>
                     </div>
                  </p>
             </div>
         </div>
     </footer>
-    <section style="text-align:center; margin:10px auto;"><p>Copyrights© MEDIS™  - <a href=<?=base_url('index.php/inicio')?>>Santiago-Chile 2018</a></p></section>
+    <section style="text-align:center; margin:10px auto;"><p>Copyrights© MEDIS™  - <a href=<?=base_url('index.php/inicio')?>>Santiago-Chile 2019</a></p></section>
 
 </div>
 </div>
